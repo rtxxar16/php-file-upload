f1b3b18163d385dc38e72f3a81720437856b0229
Kürşad <61160913+kursadHD@users.noreply.github.com>
2020-12-20 23:05:51 +0300

    Update delete.php

--- TiCloud/ğ/delete.php
+++ TiCloud/ğ/delete.php
@@ -1,7 +1,14 @@
 <?php
 
-unlink($_GET["name"]);
+if (file_exists("./".$_GET["name"])) {
+    unlink("./".$_GET["name"]);
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+}
+else {
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+}
 
-header("Location: " . $_SERVER["HTTP_REFERER"]);
+
+
 
 ?>
