10af7026bd42be4f5335617f453da9937bd04411
Kürşad <61160913+kursadHD@users.noreply.github.com>
2020-12-20 23:06:32 +0300

    Update upload.php

--- TiCloud/ğ/upload.php
+++ TiCloud/ğ/upload.php
@@ -2,8 +2,9 @@
 
 
 $file = $_FILES["file"];
+$filename = str_replace(array(" ", "[", "]", "{", "}", "+", "-", "*", "\\", "/"), array("", "", "", "", "", "", "", "", "", ""), $file["name"]);
 
-move_uploaded_file($file["tmp_name"], "cloud/" . $file["name"]);
+move_uploaded_file($file["tmp_name"], "cloud/$filename");
 
 
 header("Location: " . $_SERVER["HTTP_REFERER"]);
