c6311d913eeb150189b4182ea7a184cb18d0b8c6
Kürşad <61160913+kursadHD@users.noreply.github.com>
2020-12-24 18:52:40 +0300

    Update upload.php

--- TiCloud/ğ/upload.php
+++ TiCloud/ğ/upload.php
@@ -1,17 +1,47 @@
 <?php
+require "./func.php";
+require "./db.php";
 
+$ip = $_SERVER["REMOTE_ADDR"];
+date_default_timezone_set('Europe/Istanbul');
 
 $file = $_FILES["file"];
-$filename = str_replace(array(" ", "[", "]", "{", "}", "+", "-", "*", "\\", "/"), array("", "", "", "", "", "", "", "", "", ""), $file["name"]);
-if (file_exists("./cloud/$filename")) {
-    move_uploaded_file($file["tmp_name"], "./cloud/yeni_$filename");
+$filename = str_replace(
+    array(" ", "[", "]", "{", "}", "+", "-", "*", "\\", "/", "&", "%", "^", "'", "\"", "!", "<", ">", "|", "=", "ü", "ö", "ç", "ş", "ğ", "ı", "Ü", "Ö", "Ç", "Ş", "Ğ", "İ", "£", "#", "$", "½", "¨", "~", "`", "´", ";", ":", "@", "", "", "(", ")", ","), 
+    array("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "u", "o", "c", "s", "g", "i", "U", "O", "C", "S", "G", "I", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+    $file["name"]);
+if (isset($file)) {
+    
+    if (file_exists("./cloud/$filename")) {
+        $rand = random(5);
+        $filename = $rand."_$filename";
+        move_uploaded_file($file["tmp_name"], "./cloud/$filename");
+    }
+    else {
+        move_uploaded_file($file["tmp_name"], "./cloud/$filename");
+    }
+    
+    
+    if ($filename != "yeni_") {
+        $query = $db->prepare("INSERT INTO dosyalar SET
+        dosya_adi = ?,
+        ip_addr = ?,
+        tarih = ?,
+        boyut = ?");
+        $insert = $query->execute(array($filename, $ip, date('H:i:s  d.m.Y'), bytesto(filesize("./cloud/$filename"))));
+        if ($insert) {
+            $last_id = $db->lastInsertId();
+            header("Location: " . $_SERVER["HTTP_REFERER"]);
+        }
+        else {
+            echo "işlem başarısız oldu. lütfen tekrar deneyin. ana sayfaya yönlendiriliyorsunuz...";
+            header("Refresh: 6; url={$_SERVER['HTTP_REFERER']}");
+        }
+    }
 }
 else {
-    move_uploaded_file($file["tmp_name"], "./cloud/$filename");
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
 }
 
-
-
-header("Location: " . $_SERVER["HTTP_REFERER"]);
 
 ?>
