36485494b334ae2cbe19a693239ca5b3e9930d20
Kürşad <61160913+kursadHD@users.noreply.github.com>
2020-12-24 18:52:13 +0300

    Update delete.php

--- TiCloud/ğ/delete.php
+++ TiCloud/ğ/delete.php
@@ -1,13 +1,17 @@
 <?php
-
-if (file_exists("./cloud/".$_GET["name"])) {
-    unlink("./cloud/".$_GET["name"]);
-    header("Location: " . $_SERVER["HTTP_REFERER"]);
+require "./db.php";
+$filename = $_GET["name"];
+if ($_GET) {
+    if (file_exists("./cloud/$filename")) {
+        unlink("./cloud/$filename");
+        $query = $db->prepare("DELETE FROM dosyalar WHERE dosya_adi = :dosya_adi");
+        $delete = $query->execute(array('dosya_adi' => $filename));
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
+    else {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
 }
-else {
-    header("Location: " . $_SERVER["HTTP_REFERER"]);
-}
-
 
 
 
