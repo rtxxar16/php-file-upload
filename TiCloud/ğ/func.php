13c2894b42a16cd22cd7e5b9a9a65800b3bcd4c9
Kürşad <61160913+kursadHD@users.noreply.github.com>
2020-12-24 18:51:12 +0300

    Update func.php

--- TiCloud/ğ/func.php
+++ TiCloud/ğ/func.php
@@ -11,11 +11,22 @@ function bytesto ($bytes) {
         }
     }
     else {
-        return "$bytes bytes";
+        return "$bytes b";
     }
     
-    return $mb;
+
 }
 
+#echo bytesto(846517890);
 
+function random ($uzunluk) {
+    $str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $cikti = '';
+    for ($i = 0; $i < $uzunluk; $i++) {
+        $cikti .= $str[rand(0, strlen($str)) - 1];
+    }
+    return $cikti;
+}
+
+#echo random(5);
 ?>
