fcb7fe2ff624f9e4fef6fef5b3de5201c5b0fb95
Kürşad <61160913+kursadHD@users.noreply.github.com>
2020-12-24 19:08:11 +0300

    31

--- TiCloud/ğ/db.php
+++ TiCloud/ğ/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=kursadte_cloud;charset=utf8", "kursadte_root", "kursad12e45678");
+    $db = new PDO("mysql:host=localhost;dbname=kursadte_cloud;charset=utf8", "root", "31");
 }
 catch (PDOException $e) {
     print $e->getMessage();
