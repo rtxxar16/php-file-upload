604f86820245bb2ffdaa1cdd7ff2f2e7744c5869
Kürşad <61160913+kursadHD@users.noreply.github.com>
2020-12-24 18:50:28 +0300

    Update index.php

--- TiCloud/ğ/index.php
+++ TiCloud/ğ/index.php
@@ -1,41 +1,32 @@
 <?php
 require "./func.php";
+require "./db.php";
 ?>
 <center>
     <form method="POST" action="upload.php" enctype="multipart/form-data">
-        <input type="file" name="file"><br>
+        <input type="file" name="file" required><br>
         <input type="submit" value="Yükle">
     </form>
 </center>
 <link rel="stylesheet" type="text/css" href="style.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <?php
-    echo '<i>Dosya yüklediğiniz de aşağıda listelenecektir...</i>';
-    $dosya = scandir("cloud");
-    for ($asdas = 2; $asdas < count($dosya); $asdas++)
-
-{    
-    ?><hr>
-    
+    echo '<i>Dosya yüklediğinizde aşağıda listelenecektir...</i>';
+    $query = $db->query("SELECT * FROM dosyalar", PDO::FETCH_ASSOC);
+    if ($query->rowCount()) {
+        foreach($query as $row) {
+            
+?>
+    <hr>
     <p>
-        
-        <?php
-        echo $dosya[$asdas]."<br><i><sub>Dosya Boyutu: ".bytesto(filesize('./cloud/'.$dosya[$asdas]))."</sub></i>";
-        ?><br>
-
-        <a href="cloud/<?php 
-            echo $dosya[$asdas]; 
-        ?>" download="<?php echo $dosya[$asdas]; ?>">
-            İndir
-        </a>&nbsp;
-
-        <a href="delete.php?name=/<?php 
-            echo $dosya[$asdas]; 
-        ?>">
-            Sil
-        </a>
+        <?= $row['dosya_adi']." <i>(".$row['boyut'].")</i>"; ?><br>
+        <?= "<i><sup>{$row["tarih"]}</sup></i>"; ?><br>
+        <a href="cloud/<?= $row['dosya_adi']; ?>" download="<?= $row['dosya_adi']; ?>">İndir</a>&nbsp;
+        <a href="delete.php?name=<?= $row['dosya_adi']; ?>">Sil</a>
     </p>
     <?php
-}
+        }
+    }
 
 ?>
