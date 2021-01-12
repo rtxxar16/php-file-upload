21f33142437b64e024bea9457f5672f00dd3ca1c
Kürşad <61160913+kursadHD@users.noreply.github.com>
2021-01-12 20:10:33 +0300

    Update upload.php

--- TiCloud/ğ/upload.php
+++ TiCloud/ğ/upload.php
@@ -1,6 +1,6 @@
 <?php
-require "./func.php";
-require "./db.php";
+require("./func.php");
+require("./db.php");
 
 $ip = $_SERVER["REMOTE_ADDR"];
 date_default_timezone_set('Europe/Istanbul');
@@ -9,26 +9,29 @@ $file = $_FILES["file"];
 $filename = str_replace(
     array(" ", "[", "]", "{", "}", "+", "-", "*", "\\", "/", "&", "%", "^", "'", "\"", "!", "<", ">", "|", "=", "ü", "ö", "ç", "ş", "ğ", "ı", "Ü", "Ö", "Ç", "Ş", "Ğ", "İ", "£", "#", "$", "½", "¨", "~", "`", "´", ";", ":", "@", "", "", "(", ")", ","), 
     array("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "u", "o", "c", "s", "g", "i", "U", "O", "C", "S", "G", "I", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
-    $file["name"]);
-if (isset($file)) {
+    $file["name"]
+    );
     
-    if (file_exists("./cloud/$filename")) {
-        $rand = random(5);
-        $filename = $rand."_$filename";
-        move_uploaded_file($file["tmp_name"], "./cloud/$filename");
+if (isset($file)) {
+    $id = random(10);
+    
+    if (file_exists("./cloud/$id-$filename")) {
+        $id = random(10);
+        move_uploaded_file($file["tmp_name"], "./cloud/$id-$filename");
     }
     else {
-        move_uploaded_file($file["tmp_name"], "./cloud/$filename");
+        move_uploaded_file($file["tmp_name"], "./cloud/$id-$filename");
     }
     
     
-    if ($filename != "yeni_") {
+    if ($filename != "") {
         $query = $db->prepare("INSERT INTO dosyalar SET
+        id = ?,
         dosya_adi = ?,
         ip_addr = ?,
         tarih = ?,
         boyut = ?");
-        $insert = $query->execute(array($filename, $ip, date('H:i:s  d.m.Y'), bytesto(filesize("./cloud/$filename"))));
+        $insert = $query->execute(array($id, $filename, $ip, date('H:i:s  d.m.Y'), bytesto(filesize("./cloud/$id-$filename"))));
         if ($insert) {
             $last_id = $db->lastInsertId();
             header("Location: " . $_SERVER["HTTP_REFERER"]);
