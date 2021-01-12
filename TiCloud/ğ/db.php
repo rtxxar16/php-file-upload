869d7d010dc2485445007a6b1d5ba4458acc69e3
Tiox <60711961+Tioxs@users.noreply.github.com>
2021-01-12 20:26:54 +0300

    R bug fix

--- TiCloud/ğ/db.php
+++ TiCloud/ğ/db.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "dosyalar";
+$db_name = "cloud";
 
 try {    
     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
