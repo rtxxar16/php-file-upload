f7fbad7821e97eadf7af4543c826508595f63d00
tiox <60711961+Tioxs@users.noreply.github.com>
2021-01-25 20:59:32 +0300

    dosya adı eklendi 1.7.2

--- TiCloud/ğ/file.php
+++ TiCloud/ğ/file.php
@@ -10,6 +10,7 @@ if ($query->rowCount() > 0):
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0">
 <h4 style="text-align: center;">
+    <p>Dosya Adı: <?= $result["dosya_adi"]?> </p>
     <p>Dosya Boyutu: <?= $result["boyut"] ?></p>
     <p>Yüklenme Tarihi: <?= $result["tarih"] ?></p>
     <p><a href="download.php?id=<?= $result["id"] ?>" download="<?= $result["dosya_adi"] ?>">İNDİR</a></p>
